56f6dda51d504a283d2b08200eb851007f770aba
Igor Dominiak <igor.dominiak@netguru.pl>
2014-01-30 17:34:56 +0100

    remove underscore.string and inflections from base stack

--- vendor/assets/javascripts/js_stack.js
+++ vendor/assets/javascripts/js_stack.js
@@ -1,8 +1,6 @@
 //= require js_stack/hamlcoffee
 //= require js_stack/jsroutes
 //= require js_stack/underscore
-//= require js_stack/underscore.string
-//= require js_stack/underscore.inflections
 //= require js_stack/backbone
 //= require js_stack/backbone.stickit
 //= require js_stack/marionette
